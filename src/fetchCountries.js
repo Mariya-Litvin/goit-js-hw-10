7bde405b843f533136ea693b30cd3cdedb53258d
Mariya-Litvin <111592916+Mariya-Litvin@users.noreply.github.com>
2023-02-18 17:15:08 +0200

    change task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -6,10 +6,14 @@ function fetchCountries(name) {
   const URL = `https://restcountries.com/v3.1/name/${name}?${searchParams}`;
 
   return fetch(URL).then(response => {
-    if (!response.ok) {
+    if (response.status == 404) {
+      console.log(`failed ${response.statusText} : ${response.status}`);
+    } else if (response.status >= 200 && response.status < 300) {
+      console.log(`success ${response.statusText} : ${response.status}`);
+      return response.json();
+    } else {
       throw new Error(response.status);
     }
-    return response.json();
   });
 }
 
